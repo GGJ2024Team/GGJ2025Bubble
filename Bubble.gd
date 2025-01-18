91536a86e0c03eb1c6ee8f60899af0681251b8a7
vh_wz <vh_wz@outlook.com>
2025-01-18 16:07:07 +0800

    [feature] 支持bubble合并

--- Bubble.gd
+++ Bubble.gd
@@ -6,10 +6,10 @@ var speed = 10
 var score = 10
 var in_fan_range = false
 var bubble_strength = 1000
+var is_merged = false
 
 signal bubble_gen(score)
 signal bubble_die(score)
-
 
 func _ready():
     var game = get_tree().get_root().get_node("Game")
@@ -21,9 +21,12 @@ func _ready():
 func _process(delta):
     var node_position = global_position  
     if position.x < 0 or position.x > screen_size.x or position.y < 0 or position.y > screen_size.y:
-        emit_signal("bubble_die", -score)
-        queue_free()
+        die()
         
+func die():
+    emit_signal("bubble_die", -score)
+    queue_free()
+
 func _physics_process(delta):
     if self.in_fan_range:
         speed = 20
@@ -41,3 +44,18 @@ func _integrate_forces(state):
             # 计算推力
             var force = direction * bubble_strength
             add_force(force, Vector2(0, -1))
+
+func merge_with(other_bubble):
+    is_merged = true
+    other_bubble.is_merged = true
+    var new_position = (position + other_bubble.position) / 2
+    var merged_bubble = load("res://Bubble.tscn").instance()
+    merged_bubble.position = new_position
+    get_parent().add_child(merged_bubble)
+    other_bubble.die()  # 销毁与其合并的气泡
+    die()
+
+func _on_Area2D_area_entered(area):
+    var obj = area.get_parent()
+    if obj.is_in_group("bubble") and not is_merged:
+        merge_with(obj)
