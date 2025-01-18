e9fdd4bce8df86bc962c98dacc1475c937fcbea5
vh_wz <vh_wz@outlook.com>
2025-01-18 16:46:14 +0800

    [feature] bubble合并后变大

--- Bubble.gd
+++ Bubble.gd
@@ -7,6 +7,11 @@ var score = 10
 var in_fan_range = false
 var bubble_strength = 1000
 var is_merged = false
+var scale_factor = 1
+
+onready var sprite = $AnimatedSprite
+onready var area2d = $Area2D
+onready var coll2d = $CollisionShape2D
 
 signal bubble_gen(score)
 signal bubble_die(score)
@@ -17,12 +22,18 @@ func _ready():
     connect("bubble_die", game, "update_score")
     emit_signal("bubble_gen", score)
     add_to_group("bubble")
+    sprite.scale = Vector2(1, 1)*scale_factor
+    if scale_factor > 4:
+        die()
     
 func _process(delta):
     var node_position = global_position  
     if position.x < 0 or position.x > screen_size.x or position.y < 0 or position.y > screen_size.y:
         die()
-        
+
+func bubble_scale(factor):
+    scale_factor = factor
+    
 func die():
     emit_signal("bubble_die", -score)
     queue_free()
@@ -45,17 +56,21 @@ func _integrate_forces(state):
             var force = direction * bubble_strength
             add_force(force, Vector2(0, -1))
 
+func can_merge(other_bubble):
+    return other_bubble.is_in_group("bubble") and not is_merged and not other_bubble.is_merged and scale_factor == other_bubble.scale_factor
+    
 func merge_with(other_bubble):
     is_merged = true
     other_bubble.is_merged = true
     var new_position = (position + other_bubble.position) / 2
     var merged_bubble = load("res://Bubble.tscn").instance()
     merged_bubble.position = new_position
+    merged_bubble.bubble_scale(scale_factor*2)
     get_parent().add_child(merged_bubble)
     other_bubble.die()  # 销毁与其合并的气泡
     die()
 
 func _on_Area2D_area_entered(area):
     var obj = area.get_parent()
-    if obj.is_in_group("bubble") and not is_merged:
+    if can_merge(obj):
         merge_with(obj)
