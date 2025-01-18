53e8871e1154e9fc6a8973fce849b6dbc342a5b9
vh_wz <vh_wz@outlook.com>
2025-01-18 21:20:08 +0800

    [feature] 增加背景音乐；游戏结束后弹出返回和重新开始按钮

--- Game.gd
+++ Game.gd
@@ -1,18 +1,24 @@
 extends Node2D
 
-
+var game_started = false
 var score = 0
 onready var score_label = $UI/HUD/ColorRect/Score
 onready var http_request = $HTTPRequest
 
-func _ready():
-    pass
+signal return_to_main_menu
+signal game_restart
 
+func _ready():
+    connect("return_to_main_menu", get_parent(), "load_main_menu")
+    connect("game_restart", get_parent(), "load_game")
 
 func update_score(score_chg):
     score += score_chg
     score_label.text = "SCORE: " + str(score)
-    on_game_end()
+    if not game_started and score > 0:
+        game_started = true
+    if game_started and score <= 0:
+        on_game_end()
 
 func on_game_end():
     print("on_game_end()")
@@ -21,6 +27,14 @@ func on_game_end():
     if result != OK:
         print("Failed to request URL:", url)
     http_request.connect("request_completed", self, "_on_request_completed")
+    get_node("Character").queue_free()
+    var game_end = load("res://GameEnd.tscn").instance()
+    var return_button = game_end.get_node("ColorRect/Return")
+    var restart_button = game_end.get_node("ColorRect/Restart")
+    get_node("UI").add_child(game_end)
+    return_button.connect("pressed", self, "on_return")
+    restart_button.connect("pressed", self, "on_restart")
+    
 
 func _on_request_completed(result, response_code, headers, body):
     if response_code == 200:
@@ -28,4 +42,9 @@ func _on_request_completed(result, response_code, headers, body):
     else:
         print("Request failed with response code:", response_code)
 
+func on_return():
+    emit_signal("return_to_main_menu")
+    queue_free()
     
+func on_restart():
+    emit_signal("game_restart")
--- GameControl.gd
+++ GameControl.gd
@@ -2,17 +2,26 @@ extends Node
 
 var game_scene
 func _ready():
+    load_main_menu()
+    
+func load_main_menu():
+    var main_menu = load("res://MainMenu.tscn").instance()
+    add_child(main_menu)
     var new_game = get_node("MainMenu/MarginContainer/VBoxContainer/Newgame")
     var quit = get_node("MainMenu/MarginContainer/VBoxContainer/Quit")
     new_game.connect("pressed", self, "on_new_game_pressed")
     quit.connect("pressed", self, "on_quit_pressed")
 
+func load_game():
+    if game_scene != null:
+        game_scene.name = "GameRemoved"
+        game_scene.queue_free()
+    game_scene = load("res://Game.tscn").instance()
+    add_child(game_scene)
+   
 func on_new_game_pressed():
     get_node("MainMenu").queue_free()
-    game_scene = load("res://Game.tscn").instance()
-#    game_scene.connect("game_finished", self, "unload_game")
-#    game_scene.connect("game_restart", self, "restart_game")
-    add_child(game_scene)
+    load_game()
     
 func on_quit_pressed():
     get_tree().quit()
